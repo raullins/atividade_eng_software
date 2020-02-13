79512223b02872c15a789f6049162168914468c6
raul.lins <raul.lins@LAB2-DCX-02.labs.dcx>
2020-02-13 11:12:53 -0300

    Tomara que dê certo

--- atividade_eng_software_eclipse/src/que_desejar/Raul.java
+++ atividade_eng_software_eclipse/src/que_desejar/Raul.java
@@ -2,4 +2,20 @@ package que_desejar;
 
 public class Raul {
 
+	private int a;
+	private int b;
+	private int c;
+
+	public int soma(int a, int b) {
+
+		return a + b;
+
+	}
+
+	public static void main (String [] args) {
+		
+		
+		
+	}
+	
 }
